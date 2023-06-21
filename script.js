99c4140410914fbd63b2bdd44f80daffb704b303
Oleh <olehronin@gmail.com>
2023-06-21 16:36:51 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const numberOfFilms = +prompt('Сколько вы уже фильмов посмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотреных фильмов?', ''),
+      b = prompt('На сколько цените его?', ''),
+      c = prompt('Один из последних просмотреных фильмов?', ''),
+      d = prompt('На сколько цените его?', '');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
